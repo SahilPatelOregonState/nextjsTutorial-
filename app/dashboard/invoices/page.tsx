51b881de190526abd0fa68ceef630e441ae27482
Sahil Patel <patelsah@oregonstate.edu>
2025-08-04 19:45:29 -0400

    removed a type from this function

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,12 +4,12 @@ import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
+import { JSXElementConstructor, Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 
 export default async function Page(props:{
     searchParams?: Promise<{query?:string, page?: string}>;
-}):React.ReactNode{
+}){
    
     const searchParams = await props.searchParams;
     const query = searchParams?.query || '';
